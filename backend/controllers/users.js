882782ddd3444e3e5e71f5b632f32bd6f435bc3e
Omkar Kudale <omkar7890kudale@gmail.com>
2023-04-24 00:50:55 +0530

    Done with requiremnt 5

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -29,26 +29,81 @@ exports.firstRequirement = catchAsyncError(async (req, res, next) => {
     .json({ success: true, responses: users.length, users: users });
 });
 
-
-
 // 2. Male Users which have phone price greater than 10,000.
 exports.secondRequirement = catchAsyncError(async (req, res, next) => {
-    const query = {
-      gender: "Male",
-      phone_price: { $gt:"10000" },
-    }; //We have a fixed a requirement thats why we are giving fixed query in the form of variable
-    const users = await User.find(query);
-    if (!users) {
-      return next(new ErrorHandler("Cannot find users", 400));
-    }
-  
-    res
-      .status(200)
-      .json({ success: true, responses: users.length, users: users });
-  });
+  const query = {
+    gender: "Male",
+    phone_price: { $gt: "10000" },
+  }; //We have a fixed a requirement thats why we are giving fixed query in the form of variable
+  const users = await User.find(query);
+  if (!users) {
+    return next(new ErrorHandler("Cannot find users", 400));
+  }
 
-
+  res
+    .status(200)
+    .json({ success: true, responses: users.length, users: users });
+});
 
 // 3. Users whose last name starts with “M” and has a quote character length greater than 15 and email includes his/her last name.
+
+exports.thirdRequirement = catchAsyncError(async (req, res, next) => {
+  const query = {
+    last_name: { $regex: /^M/i }, // match last name starting with M (case-insensitive)
+    email: { $regex: /M$/i }, // match email ending with M (case-insensitive)
+    $expr: { $gt: [{ $strLenCP: "$quote" }, 15] }, // match quote length greater than 15
+  };
+  const users = await User.find(query);
+  if (!users) {
+    return next(new ErrorHandler("Cannot find users", 400));
+  }
+
+  res
+    .status(200)
+    .json({ success: true, responses: users.length, users: users });
+});
+
 // 4. Users which have a car of brand “BMW”, “Mercedes” or “Audi” and whose email does not include any digit.
+
+exports.fourthRequirement = catchAsyncError(async (req, res, next) => {
+  const query = {
+    car: { $in: ["BMW", "Mercedes", "Audi"] },
+    email: { $not: { $regex: /\d/ } },
+  };
+  const users = await User.find(query);
+  if (!users) {
+    return next(new ErrorHandler("Cannot find users", 400));
+  }
+
+  res
+    .status(200)
+    .json({ success: true, responses: users.length, users: users });
+});
 // 5. Show the data of top 10 cities which have the highest number of users and their average income.
+exports.fifthRequirement = catchAsyncError(async (req, res, next) => {
+  const pipeline = [
+    {
+      $group: {
+        _id: "$city",
+        count: { $sum: 1 },
+        totalIncome: { $sum: { $toDouble: { $substr: ["$income", 1, -1] } } },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        city: "$_id",
+        count: 1,
+        averageIncome: { $divide: ["$totalIncome", "$count"] },
+      },
+    },
+    { $sort: { count: -1 } },
+    { $limit: 10 },
+  ];
+
+  const result = await User.aggregate(pipeline);
+
+  res
+    .status(200)
+    .json({ success: true, responses: result.length, data: result });
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { fetchAllUsers,firstRequirement, secondRequirement} = require("../controllers/users")
+const { fetchAllUsers,firstRequirement, secondRequirement, thirdRequirement, fourthRequirement, fifthRequirement} = require("../controllers/users")
 
 const router = express.Router()
 
@@ -7,5 +7,8 @@ const router = express.Router()
 router.route("/users").get(fetchAllUsers)
 router.route("/users/req-1").get(firstRequirement)
 router.route("/users/req-2").get(secondRequirement)
+router.route("/users/req-3").get(thirdRequirement)
+router.route("/users/req-4").get(fourthRequirement)
+router.route("/users/req-5").get(fifthRequirement)
 
 module.exports = router
